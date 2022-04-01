130ee3efcad09eaff34da3455790afea8dd3328d
Josh <adesanyajoshua@ymail.com>
2022-04-01 18:33:29 +0100

    challenge-2

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,19 +5,36 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    uint256 public constant tokensPerEth = 100;
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    YourToken public yourToken;
 
-  YourToken public yourToken;
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() external payable {
+        require(msg.value > 0, "Insufficient Ether provided");
 
-  // ToDo: create a payable buyTokens() function:
+        uint256 tokens = tokensPerEth * msg.value;
+        yourToken.transfer(msg.sender, tokens);
+        emit BuyTokens(msg.sender, msg.value, tokens);
+    }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public onlyOwner {
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Failed to withdraw money");
+    }
 
-  // ToDo: create a sellTokens() function:
+    // ToDo: create a sellTokens() function:
+    function sellTokens(uint256 amount) public {
+        yourToken.transferFrom(msg.sender, address(this), amount);
 
+        uint256 ethAmount = (amount / tokensPerEth);
+
+        (bool success, ) = msg.sender.call{value: ethAmount}("");
+    }
 }
